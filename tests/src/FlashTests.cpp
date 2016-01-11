d5e280befc8b93f54c75cc41cae843b4d36b81b1
Eric Yanush <ericyanush@gmail.com>
2016-01-10 22:12:21 -0600

    Refactor common code from FlashTests

--- tests/src/FlashTests.cpp
+++ tests/src/FlashTests.cpp
@@ -9,19 +9,28 @@
 #include "gtest/gtest.h"
 #include "Flash.hpp"
 
+
+/**
+ Note: upon initializtion of the test fixture, the flash member object is initialized
+ to all zeros. The flashIsInInitState function checks to see if the flash member object
+ is still all zeros.
+ */
 class FlashTest : public ::testing::Test {
     
 protected:
     
     virtual void SetUp() {
-        flash = (Flash*) malloc(sizeof(Flash));
     }
     
     virtual void TearDown() {
-        free(flash);
     }
     
-    Flash* flash;
+    Flash flash{0};
+    const Flash untouched{0};
+    
+    bool flashIsInInitState() {
+        return (memcmp(&flash, &untouched, sizeof(Flash)) == 0);
+    }
 };
 
 /** Ensure layout of Flash object adheres with Register Map
@@ -39,21 +48,22 @@ TEST_F(FlashTest, TestFlashRegLayout) {
 }
 
 TEST_F(FlashTest, TestLatencySet) {
-    flash->AC = 0;
-    flash->setLatency(FlashWait::one);
-    ASSERT_EQ(static_cast<uint32_t>(FlashWait::one), flash->AC);
+    flash.AC = 0;
+    flash.setLatency(FlashWait::one);
+    ASSERT_EQ(static_cast<uint32_t>(FlashWait::one), flash.AC);
 }
 
 TEST_F(FlashTest, TestPrefetchEnable) {
-    flash->AC = 0;
-    flash->enablePrefetch();
-    ASSERT_EQ(1 << 4, flash->AC);
+    flash.enablePrefetch();
+    ASSERT_EQ(1 << 4, flash.AC);
+    flash.AC &= ~(1 << 4);
+    ASSERT_EQ(true, flashIsInInitState());
 }
 
 TEST_F(FlashTest, TestPrefetchDisable) {
-    flash->AC = 0;
-    flash->AC |= (1 << 4);
-    flash->disablePrefetch();
-    ASSERT_EQ(0, flash->AC);
+    flash.AC |= (1 << 4); //Manually enable the prefetch
+    flash.disablePrefetch();
+    ASSERT_EQ(0, flash.AC);
+    ASSERT_EQ(true, flashIsInInitState());
 }
 
