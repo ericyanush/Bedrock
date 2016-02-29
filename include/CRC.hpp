a71b771fba0059c2a8b3b226c1d1e47546583694
Eric Yanush <ericyanush@gmail.com>
2016-02-29 13:06:03 -0600

    Add CRC computation unit class

---
+++ include/CRC.hpp
@@ -0,0 +1,36 @@
+//
+//  CRC.hpp
+//  Bedrock
+//
+//  Created by Eric Yanush on 2016-02-22.
+//  Copyright © 2016 EricYanush. All rights reserved.
+//
+
+#ifndef CRC_h
+#define CRC_h
+
+#include <stdint.h>
+#include "types.hpp"
+
+class CRC {
+public:
+    dev_reg32_t DR;
+    dev_reg32_t IDR;
+    dev_reg32_t CR;
+    dev_reg32_t INIT;
+    dev_reg32_t POL;
+    
+    void reset() {
+        CR |= 0x1;
+    }
+    void addWord(uint32_t word) {
+        DR = word;
+    }
+    uint32_t getResult() {
+        return DR;
+    }
+};
+
+using CRCProvider = CRC& (*)(void);
+
+#endif /* CRC_h */
