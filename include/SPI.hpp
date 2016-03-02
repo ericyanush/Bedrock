3b402e0be0321f6c662e4255b2f8b336c1ad3e0a
Eric Yanush <ericyanush@gmail.com>
2016-03-02 15:47:04 -0600

    Add SPI peripheral class

---
+++ include/SPI.hpp
@@ -0,0 +1,214 @@
+//
+//  SPI.hpp
+//  Bedrock
+//
+//  Created by Eric Yanush on 2016-02-28.
+//  Copyright © 2016 EricYanush. All rights reserved.
+//
+
+#ifndef SPI_h
+#define SPI_h
+
+#include "types.hpp"
+
+namespace Bedrock {
+    
+    class SPI {
+    public:
+        dev_reg16_t CR1;
+        dev_reg16_t __pad1; //anonymous padding
+        dev_reg16_t CR2;
+        dev_reg16_t __pad2; //anonymous padding
+        dev_reg16_t SR;
+        dev_reg16_t __pad3; //anonymous padding
+        dev_reg16_t DR;
+        dev_reg16_t __pad4; //anonymous padding
+        dev_reg16_t CRCPR;
+        dev_reg16_t __pad5; //anonymous padding
+        dev_reg16_t RXCRCR;
+        dev_reg16_t __pad6; //anonymous padding
+        dev_reg16_t TXCRCR;
+        dev_reg16_t __pad7; //anonymous padding
+        dev_reg16_t I2SCFGR;
+        dev_reg16_t __pad8; //anonymous padding
+        dev_reg16_t I2SPR;
+        dev_reg16_t __pad9; //anonymous padding
+        
+        enum class FrameFormat {
+            MSBFirst = 0,
+            LSBFirst = 1
+        };
+        
+        void setFrameFormat(FrameFormat format) {
+            if (format == FrameFormat::LSBFirst) {
+                CR1 |= (ENABLE << 7);
+            }
+            else {
+                CR1 &= ~(ENABLE << 7);
+            }
+        }
+        
+        FrameFormat getFrameFormat() {
+            uint8_t f = (CR1 >> 7) & 0x1;
+            return static_cast<FrameFormat>(f);
+        }
+        
+        void enable() {
+            CR1 |= (ENABLE << 6);
+        }
+        void disable() {
+            CR1 &= ~(ENABLE << 6);
+        }
+        
+        enum class BaudRatePrescaler
+        {
+            Div2   = 0b000,
+            Div4   = 0b001,
+            Div8   = 0b010,
+            Div16  = 0b011,
+            Div32  = 0b100,
+            Div64  = 0b101,
+            Div128 = 0b110,
+            Div256 = 0b111
+        };
+        void setBaudRatePrescaler(BaudRatePrescaler pre)
+        {
+            CR1 &= ~(0b111 << 3); //clear current value
+            CR1 |= static_cast<uint32_t>(pre) << 3;
+        }
+        BaudRatePrescaler getBaudRatePrescaler()
+        {
+            return static_cast<BaudRatePrescaler>((CR1 >> 3) & 0b111);
+        }
+        
+        enum class DeviceMode
+        {
+            Slave  = 0,
+            Master = 1
+        };
+        
+        
+        void setDeviceMode(DeviceMode mode)
+        {
+            if (mode == DeviceMode::Slave)
+            {
+                CR1 &= ~(ENABLE << 2);
+            }
+            else
+            {
+                CR1 |= (ENABLE << 2);
+            }
+        }
+        
+        DeviceMode getDeviceMode()
+        {
+            return static_cast<DeviceMode>((CR1 >> 2) & 0x1);
+        }
+        
+        enum class SPIMode {
+            Zero  = 0b00,
+            One   = 0b01,
+            Two   = 0b10,
+            Three = 0b11
+        };
+        
+        void setSPIMode(SPIMode mode) {
+            CR1 &= ~(0b11); //Clear current value;
+            CR1 |= static_cast<uint32_t>(mode);
+        }
+        SPIMode getSPIMode() {
+            return static_cast<SPIMode>(CR1 & 0b11);
+        }
+        
+        enum class TransmitSize {
+            Bits4  = 0b0011,
+            Bits5  = 0b0100,
+            Bits6  = 0b0101,
+            Bits7  = 0b0110,
+            Bits8  = 0b0111,
+            Bits9  = 0b1000,
+            Bits10 = 0b1001,
+            Bits11 = 0b1010,
+            Bits12 = 0b1011,
+            Bits13 = 0b1100,
+            Bits14 = 0b1101,
+            Bits15 = 0b1110,
+            Bits16 = 0b1111
+        };
+        
+        void setTransmitSize(TransmitSize size) {
+            CR2 &= ~(0b1111 << 8);
+            CR2 |= static_cast<uint32_t>(size) << 8;
+        }
+        
+        TransmitSize getTransmitSize() {
+            return static_cast<TransmitSize>((CR2 >> 8) & 0b1111);
+        }
+        
+        void enableTXBufferEmptyInterrupt() {
+            CR2 |= ENABLE << 7;
+        }
+        void disableTXBufferEmptyInterrupt() {
+            CR2 &= ~(ENABLE << 7);
+        }
+        
+        void enableRXBufferNotEmptyInterrupt() {
+            CR2 |= ENABLE << 6;
+        }
+        void disableRXBufferNotEmptyInterrupt() {
+            CR2 &= ~(ENABLE << 6);
+        }
+        
+        void enableErrorInterrupt() {
+            CR2 |= ENABLE << 5;
+        }
+        void disableErrorInterrupt() {
+            CR2 &= ~(ENABLE << 5);
+        }
+        
+        void enableTXDMA() {
+            CR2 |= ENABLE << 1;
+        }
+        void disableTXDMA() {
+            CR2 &= ~(ENABLE << 1);
+        }
+        
+        void enableRXDMA() {
+            CR2 |= (ENABLE << 0);
+        }
+        void disableRXDMA() {
+            CR2 &= ~(ENABLE << 0);
+        }
+        
+        enum class BufferLevel {
+            Empty   = 0b00,
+            Quarter = 0b01,
+            Half    = 0b10,
+            Full    = 0b11
+        };
+        
+        BufferLevel getTXLevel() {
+            return static_cast<BufferLevel>((SR >> 11) & 0b11);
+        }
+        
+        BufferLevel getRXLevel() {
+            return static_cast<BufferLevel>((SR >> 9) & 0b11);
+        }
+        
+        bool isBusy() {
+            return (SR >> 7) & 0x1;
+        }
+        
+        bool txEmpty() {
+            return (SR >> 1) & 0x1;
+        }
+        
+        bool rxNotEmpty() {
+            return (SR >> 0) & 0x1;
+        }
+        
+    };
+    
+}
+
+#endif /* SPI_h */
---
+++ tests/src/SPITests.cpp
@@ -0,0 +1,258 @@
+//
+//  SPITests.cpp
+//  Bedrock tests
+//
+//  Created by Eric Yanush on 2016-02-28.
+//  Copyright © 2016 EricYanush. All rights reserved.
+//
+
+#include "SPI.hpp"
+#include "gtest/gtest.h"
+
+class SPITests : public ::testing::Test {
+protected:
+    Bedrock::SPI spi{0};
+    
+    void SetUp()
+    {
+        //set register reset values that are not 0
+        spi.CR2 = 0x700;
+        spi.SR = 0x2;
+        spi.CRCPR = 0x7;
+        spi.I2SPR = 0x2;
+    }
+};
+
+TEST_F(SPITests, TestRegisterLayout)
+{
+    ASSERT_EQ(0x00, offsetof(Bedrock::SPI, CR1));
+    ASSERT_EQ(0x04, offsetof(Bedrock::SPI, CR2));
+    ASSERT_EQ(0x08, offsetof(Bedrock::SPI, SR));
+    ASSERT_EQ(0x0C, offsetof(Bedrock::SPI, DR));
+    ASSERT_EQ(0x10, offsetof(Bedrock::SPI, CRCPR));
+    ASSERT_EQ(0x14, offsetof(Bedrock::SPI, RXCRCR));
+    ASSERT_EQ(0x18, offsetof(Bedrock::SPI, TXCRCR));
+    ASSERT_EQ(0x1C, offsetof(Bedrock::SPI, I2SCFGR));
+    ASSERT_EQ(0x20, offsetof(Bedrock::SPI, I2SPR));
+    
+    ASSERT_EQ(0x24, sizeof(Bedrock::SPI));
+}
+
+TEST_F(SPITests, TestSetFrameFormat)
+{
+    spi.setFrameFormat(Bedrock::SPI::FrameFormat::LSBFirst);
+    ASSERT_EQ(0x1 << 7, spi.CR1);
+    spi.CR1 &= ~(0x1 << 7);
+    
+    spi.setFrameFormat(Bedrock::SPI::FrameFormat::MSBFirst);
+    ASSERT_EQ(0x0, spi.CR1);
+}
+
+TEST_F(SPITests, TestGetFrameFormat)
+{
+    using Format = Bedrock::SPI::FrameFormat;
+    
+    ASSERT_EQ(Format::MSBFirst, spi.getFrameFormat());
+    spi.setFrameFormat(Format::LSBFirst);
+    ASSERT_EQ(Format::LSBFirst, spi.getFrameFormat());
+}
+
+TEST_F(SPITests, TestEnable)
+{
+    spi.enable();
+    ASSERT_EQ(0x1 << 6, spi.CR1);
+}
+
+TEST_F(SPITests, TestDisable)
+{
+    spi.CR1 |= 0x1 << 6;
+    spi.disable();
+    
+    ASSERT_EQ(0, spi.CR1);
+}
+
+TEST_F(SPITests, TestSetPrescaler)
+{
+    using Pre = Bedrock::SPI::BaudRatePrescaler;
+    
+    spi.setBaudRatePrescaler(Pre::Div32);
+    ASSERT_EQ(static_cast<uint16_t>(Pre::Div32) << 3, spi.CR1);
+    spi.setBaudRatePrescaler(Pre::Div2);
+    ASSERT_EQ(static_cast<uint16_t>(Pre::Div2) << 3, spi.CR1);
+    spi.setBaudRatePrescaler(Pre::Div256);
+    ASSERT_EQ(static_cast<uint16_t>(Pre::Div256) << 3, spi.CR1);
+}
+
+TEST_F(SPITests, TestGetPrescaler)
+{
+    using Pre = Bedrock::SPI::BaudRatePrescaler;
+    
+    ASSERT_EQ(Pre::Div2, spi.getBaudRatePrescaler());
+    spi.setBaudRatePrescaler(Pre::Div64);
+    ASSERT_EQ(Pre::Div64, spi.getBaudRatePrescaler());
+}
+
+TEST_F(SPITests, TestSetDeviceMode)
+{
+    spi.setDeviceMode(Bedrock::SPI::DeviceMode::Master);
+    ASSERT_EQ(0x1 << 2, spi.CR1);
+    spi.setDeviceMode(Bedrock::SPI::DeviceMode::Slave);
+    ASSERT_EQ(0x0, spi.CR1);
+}
+
+TEST_F(SPITests, TestGetDeviceMode)
+{
+    using DevMode = Bedrock::SPI::DeviceMode;
+    
+    ASSERT_EQ(DevMode::Slave, spi.getDeviceMode());
+    spi.setDeviceMode(DevMode::Master);
+    ASSERT_EQ(DevMode::Master, spi.getDeviceMode());
+}
+
+TEST_F(SPITests, TestSetSPIMode)
+{
+    spi.setSPIMode(Bedrock::SPI::SPIMode::Two);
+    ASSERT_EQ(0b10, spi.CR1);
+    spi.setSPIMode(Bedrock::SPI::SPIMode::Zero);
+    ASSERT_EQ(0b00, spi.CR1);
+    spi.setSPIMode(Bedrock::SPI::SPIMode::Three);
+    ASSERT_EQ(0b11, spi.CR1);
+    spi.setSPIMode(Bedrock::SPI::SPIMode::One);
+    ASSERT_EQ(0b01, spi.CR1);
+}
+
+TEST_F(SPITests, TestGetSPIMode)
+{
+    using Mode = Bedrock::SPI::SPIMode;
+    
+    ASSERT_EQ(Mode::Zero, spi.getSPIMode());
+    spi.setSPIMode(Mode::Two);
+    ASSERT_EQ(Mode::Two, spi.getSPIMode());
+}
+
+TEST_F(SPITests, TestSetTransmitSize)
+{
+    spi.setTransmitSize(Bedrock::SPI::TransmitSize::Bits14);
+    ASSERT_EQ(0b1101 << 8, spi.CR2);
+    spi.setTransmitSize(Bedrock::SPI::TransmitSize::Bits7);
+    ASSERT_EQ(0b0110 << 8, spi.CR2);
+}
+
+TEST_F(SPITests, TestGetTransmitSize)
+{
+    using Size = Bedrock::SPI::TransmitSize;
+    
+    ASSERT_EQ(Size::Bits8, spi.getTransmitSize());
+    spi.setTransmitSize(Size::Bits10);
+    ASSERT_EQ(Size::Bits10, spi.getTransmitSize());
+    spi.setTransmitSize(Size::Bits15);
+    ASSERT_EQ(Size::Bits15, spi.getTransmitSize());
+    spi.setTransmitSize(Size::Bits4);
+    ASSERT_EQ(Size::Bits4, spi.getTransmitSize());
+}
+
+TEST_F(SPITests, TestEnableTXBufferEmptyInterrupt)
+{
+    spi.enableTXBufferEmptyInterrupt();
+    ASSERT_EQ(0x1 << 7, spi.CR2 & (0x1 << 7));
+}
+
+TEST_F(SPITests, TestDisableTXBufferEmptyInterrupt)
+{
+    spi.CR2 |= (0x1 << 7);
+    spi.disableTXBufferEmptyInterrupt();
+    ASSERT_EQ(0x0, spi.CR2 & (0x1 << 7));
+}
+
+TEST_F(SPITests, TestEnableRXBufferNEInterrupt)
+{
+    spi.enableRXBufferNotEmptyInterrupt();
+    ASSERT_EQ(0x1 << 6, spi.CR2 & (0x1 << 6));
+}
+
+TEST_F(SPITests, TestDisableRXBufferNEInterrupt)
+{
+    spi.CR2 |= 0x1 << 6;
+    spi.disableRXBufferNotEmptyInterrupt();
+    ASSERT_EQ(0x0, spi.CR2 & (0x1 << 6));
+}
+
+TEST_F(SPITests, TestEnableErrorInterrupt)
+{
+    spi.enableErrorInterrupt();
+    ASSERT_EQ(0x1 << 5, spi.CR2 & (0x1 << 5));
+}
+
+TEST_F(SPITests, TestDisableErrorInterrupt)
+{
+    spi.CR2 |= 0x1 << 5;
+    spi.disableErrorInterrupt();
+    ASSERT_EQ(0x0, spi.CR2 & (0x1 << 5));
+}
+
+TEST_F(SPITests, TestEnableTXDMA)
+{
+    spi.enableTXDMA();
+    ASSERT_EQ(0x1 << 1, spi.CR2 & (0x1 << 1));
+}
+
+TEST_F(SPITests, TestDisableTXDMA)
+{
+    spi.CR2 |= (0x1 << 1);
+    spi.disableTXDMA();
+    ASSERT_EQ(0x0, spi.CR2 & (0x1 << 1));
+}
+
+TEST_F(SPITests, TestEnableRXDMA)
+{
+    spi.enableRXDMA();
+    ASSERT_EQ(0x1, spi.CR2 & 0x1);
+}
+
+TEST_F(SPITests, TestDisableRXDMA)
+{
+    spi.CR2 |= (0x1);
+    spi.disableRXDMA();
+    ASSERT_EQ(0x0, spi.CR2 & 0x1);
+}
+
+TEST_F(SPITests, TestGetTXBufferLevel)
+{
+    using Level = Bedrock::SPI::BufferLevel;
+    ASSERT_EQ(Level::Empty, spi.getTXLevel());
+    spi.SR = 0b11 << 11;
+    ASSERT_EQ(Level::Full, spi.getTXLevel());
+    spi.SR = 0b10 << 11;
+    ASSERT_EQ(Level::Half, spi.getTXLevel());
+}
+
+TEST_F(SPITests, TestGetRXBufferLevel)
+{
+    using Level = Bedrock::SPI::BufferLevel;
+    ASSERT_EQ(Level::Empty, spi.getRXLevel());
+    spi.SR = 0b11 << 9;
+    ASSERT_EQ(Level::Full, spi.getRXLevel());
+    spi.SR = 0b01 << 9;
+    ASSERT_EQ(Level::Quarter, spi.getRXLevel());
+}
+
+TEST_F(SPITests, TestBusy)
+{
+    ASSERT_FALSE(spi.isBusy());
+    spi.SR = 0x1 << 7;
+    ASSERT_TRUE(spi.isBusy());
+}
+
+TEST_F(SPITests, TestTXEmpty)
+{
+    ASSERT_TRUE(spi.txEmpty());
+    spi.SR = 0;
+    ASSERT_FALSE(spi.txEmpty());
+}
+
+TEST_F(SPITests, TestRXNotEmpty)
+{
+    ASSERT_FALSE(spi.rxNotEmpty());
+    spi.SR = 0x1;
+    ASSERT_TRUE(spi.rxNotEmpty());
+}
