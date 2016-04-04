b4440fdd0d83044d57cfe2a4eb62a3d08d7230bf
Eric Yanush <ericyanush@gmail.com>
2016-04-04 02:16:16 -0600

    Add input capture handling methods to GPTimer class

--- include/GPTimer.hpp
+++ include/GPTimer.hpp
@@ -72,6 +72,60 @@ namespace Bedrock {
             SR &= ~(1 << 6);
         }
         
+        enum class CapCompSelection : uint8_t
+        {
+            OUTPUT = 0b00,
+            IN_DEF = 0b01,
+            IN_ALT = 0b10,
+            IN_TRC = 0b11
+        };
+        
+        void setCapCompSelection(uint8_t channel, CapCompSelection sel)
+        {
+            uint8_t regIndex = channel/3;
+            uint8_t bitOffset = ((channel - 1) % 2) * 8;
+            
+            CCMR[regIndex] &= ~(0b11 << bitOffset);
+            CCMR[regIndex] |= static_cast<uint8_t>(sel) << bitOffset;
+        }
+        
+        void enableCapCompChannel(uint8_t channel)
+        {
+            uint8_t bitOffset = (channel - 1) * 4;
+            CCER |= (ENABLE << bitOffset);
+        }
+        
+        void disableCapCompChannel(uint8_t channel)
+        {
+            uint8_t bitOffset = (channel - 1) * 4;
+            CCER &= ~(ENABLE << bitOffset);
+        }
+        
+        width_t readInputCaptureChannel(uint8_t channel)
+        {
+            return CCR[channel];
+        }
+        
+        void enableCaptureEvent(uint8_t channel)
+        {
+            EGR |= ENABLE << channel;
+        }
+        
+        void disableCaptureEvent(uint8_t channel)
+        {
+            EGR &= ~(ENABLE << channel);
+        }
+        
+        bool captureCompleteForChannel(uint8_t channel)
+        {
+            return (SR >> channel) & 0x1;
+        }
+        
+        void ackCaptureForChannel(uint8_t channel)
+        {
+            SR &= ~(0x1 << channel);
+        }
+        
         dev_reg32_t CR1;
         dev_reg32_t CR2;
         dev_reg32_t SMCR;
